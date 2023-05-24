e3e7753d9e323a5391ca71c397fd8146bce04677
unknown <henna.tuunainen@gmail.com>
2023-05-24 15:00:08 +0300

    Add 4.8 blogilistan testit, step 2

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -16,27 +16,43 @@ describe('when there is initially some blogs saved', () => {
     await Blog.insertMany(helper.initialBlogs)
   })
 
+  /* 4.8 blogilistan testit, step 1
+  Tee SuperTest-kirjastolla testit blogilistan osoitteeseen /api/blogs tapahtuvalle HTTP GET ‑pyynnölle. Testaa, että sovellus palauttaa oikean määrän JSON-muotoisia blogeja.
+
+  Kun testi on valmis, refaktoroi operaatio käyttämään promisejen sijaan async/awaitia.
+
+  Huomaa, että joudut tekemään koodiin materiaalin tapaan hieman muutoksia (mm. testausympäristön määrittely), jotta saat järkevästi tehtyä omaa tietokantaa käyttäviä API-tason testejä.
+
+  Varoitus: Jos huomaat kirjoittavasi sekaisin async/awaitia ja then-kutsuja, on 99-prosenttisen varmaa, että teet jotain väärin. Käytä siis jompaakumpaa tapaa, älä missään tapauksessa "varalta" molempia.
+
+  HUOM: Materiaalissa käytetään muutamaan kertaan matcheria toContain kun tarkastetaan, onko jokin arvo taulukossa. Kannattaa huomata, että metodi käyttää samuuden vertailuun ===-operaattoria ja olioiden kohdalla tämä ei ole useinkaan se mitä halutaan. Parempi vaihtoehto onkin toContainEqual. Tosin mallivastauksissa ei vertailla kertaakaan olioita matcherien avulla, joten ilmankin selviää varsin hyvin.
+  */
+
   test('three blogs are returned as json', async () => {
+    // supertest osuus
     const response = await api.get('/api/blogs')
       .expect(200)
       .expect('Content-Type', /application\/json/)
 
+    // hyödynnetään supertestillä saatua responsea jesti expectissä
     expect(response.body).toHaveLength(helper.initialBlogs.length)
   })
 
-  test('all blogs are returned', async () => {
+  /* 4.9 blogilistan testit, step 2
+  Tee testi, joka varmistaa että palautettujen blogien identifioivan kentän tulee olla nimeltään id. Oletusarvoisestihan tietokantaan talletettujen olioiden tunnistekenttä on _id. Olion kentän olemassaolon tarkastaminen onnistuu Jestin matcherillä toBeDefined.
+
+  Muuta koodia siten, että testi menee läpi. Osassa 3 käsitelty toJSON on sopiva paikka parametrin id määrittelyyn.
+  */
+
+  test('the identifying field for returned blogs is called id', async () => {
     const response = await api.get('/api/blogs')
+      .expect(200)
 
-    expect(response.body).toHaveLength(helper.initialBlogs.length)
-  })
+    console.log(response.body)
 
-  test('a specific blog is within the returned blogs', async () => {
-    const response = await api.get('/api/blogs')
-
-    const titles = response.body.map(r => r.title)
-    expect(titles).toContain(
-      'HTML is low'
-    )
+    response.body.forEach(blog => {
+      expect(blog.id).toBeDefined()
+    })
   })
 })
 
