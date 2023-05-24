3d2908608d7700672b89f5411683192791339755
unknown <henna.tuunainen@gmail.com>
2023-05-24 19:14:14 +0300

    Fix 4.12* blogilistan testit, step 5

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -32,76 +32,90 @@ describe('when there is initially some blogs saved', () => {
 
     response.body.forEach(blog => expect(blog.id).toBeDefined())
   })
-})
 
-describe('addition of a new note', () => {
-  beforeEach(async () => {
-    await Blog.deleteMany({})
-    await Blog.insertMany(helper.initialBlogs)
+  describe('addition of a new note', () => {
+
+    test('succeed with valid data', async () => {
+      const newBlog = {
+        title: 'Test is test na naaa naa na na',
+        author: 'Person999',
+        url: 'url999',
+        likes: 999,
+      }
+
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+      const blogsAtEnd = await helper.blogsInDb()
+      expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+      const titles = blogsAtEnd.map(b => b.title)
+      expect(titles).toContain('Test is test na naaa naa na na')
+    })
+
+    test('likes default value set to 0 if no other value given', async () => {
+      const newBlog = {
+        title: 'Test zero',
+        author: 'Person999',
+        url: 'url999',
+      }
+
+      const response = await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+      // console.log(response.body)
+
+      expect(response.body.likes).toBe(0)
+    })
+
+    /*
+    4.12*: blogilistan testit, step 5
+    Tee testit blogin lisäämiselle eli osoitteeseen /api/blogs tapahtuvalle HTTP POST ‑pyynnölle jotka varmistavat, että jos uusi blogi ei sisällä kenttää title tai kenttää url, pyyntöön vastataan statuskoodilla 400 Bad Request.
+  
+    Laajenna toteutusta siten, että testit menevät läpi.
+    */
+
+    test('fails with statuscode 400 if title or url is invalid', async () => {
+      const newBlog = {
+        author: 'Person999',
+      }
+
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(400)
+
+      const blogsAtEnd = await helper.blogsInDb()
+
+      // console.log(blogsAtEnd)
+
+      expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+
+    })
   })
 
-  test('succeed with valid data', async () => {
-    const newBlog = {
-      title: 'Test is test na naaa naa na na',
-      author: 'Person999',
-      url: 'url999',
-      likes: 999,
-    }
+  describe('deletion of a blog', () => {
+    test('succeeds with status code 204 if id is valid', async () => {
+      const blogsAtStart = await helper.blogsInDb()
+      const blogToDelete = blogsAtStart[0]
 
-    await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(201)
-      .expect('Content-Type', /application\/json/)
+      await api
+        .delete(`/api/blogs/${blogToDelete.id}`)
+        .expect(204)
 
-    const blogsAtEnd = await helper.blogsInDb()
-    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+      const blogsAtEnd = await helper.blogsInDb()
 
-    const titles = blogsAtEnd.map(b => b.title)
-    expect(titles).toContain('Test is test na naaa naa na na')
-  })
+      expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1)
 
-  test('likes default value set to 0 if no other value given', async () => {
-    const newBlog = {
-      title: 'Test zero',
-      author: 'Person999',
-      url: 'url999',
-    }
-
-    const response = await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(201)
-      .expect('Content-Type', /application\/json/)
-
-    // console.log(response.body)
-
-    expect(response.body.likes).toBe(0)
-  })
-
-  /*
-  4.12*: blogilistan testit, step 5
-  Tee testit blogin lisäämiselle eli osoitteeseen /api/blogs tapahtuvalle HTTP POST ‑pyynnölle jotka varmistavat, että jos uusi blogi ei sisällä kenttää title tai kenttää url, pyyntöön vastataan statuskoodilla 400 Bad Request.
-
-  Laajenna toteutusta siten, että testit menevät läpi.
-  */
-
-  test('fails with statuscode 400 if title or url is invalid', async () => {
-    const newBlog = {
-      author: 'Person999',
-    }
-
-    await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(400)
-
-    const blogsAtEnd = await helper.blogsInDb()
-
-    // console.log(blogsAtEnd)
-
-    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
-
+      const titles = blogsAtEnd.map(r => r.title)
+      expect(titles).not.toContain(blogToDelete.title)
+    })
   })
 })
 
