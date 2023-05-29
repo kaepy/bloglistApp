dcc5e2118dfe80678776a075ac4957d11a0d1ab7
kaepy <henna.tuunainen@gmail.com>
2023-05-29 21:30:12 +0300

    Add 4.18 blogilistan laajennus, step 6

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 mongoose.set('strictQuery', false)
 
@@ -30,6 +31,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
